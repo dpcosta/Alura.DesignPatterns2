85a244bd44b8b86dd9cf5ce4353e7645ad92f797
dpcosta <dpcosta@gmail.com>
2018-03-29 16:03:59 -0300

    Add project files.

---
+++ Alura.DesignPatterns2/Program.cs
@@ -0,0 +1,21 @@
+﻿using Alura.DesignPatterns2.Cap1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.DesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "selec * from tabela";
+        }
+    }
+}
