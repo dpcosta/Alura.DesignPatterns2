3f77867002a781674188a5d117f3bebcade86b92
dpcosta <dpcosta@gmail.com>
2018-03-29 16:32:46 -0300

    Arquivos da aula 2.

---
+++ Alura.DesignPatterns2/Cap2/INota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.DesignPatterns2.Cap2
+{
+    interface INota
+    {
+        int Frequencia { get; }
+    }
+}
--- Alura.DesignPatterns2/Program.cs
+++ Alura.DesignPatterns2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Alura.DesignPatterns2.Cap1;
+using Alura.DesignPatterns2.Cap2;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -13,9 +14,18 @@ namespace Alura.DesignPatterns2
     {
         static void Main(string[] args)
         {
-            IDbConnection conexao = new ConnectionFactory().GetConnection();
-            IDbCommand comando = conexao.CreateCommand();
-            comando.CommandText = "selec * from tabela";
+            NotasMusicais notas = new NotasMusicais();
+            IList<INota> musica = new List<INota>()
+            {
+                notas.Pega("do"),
+                notas.Pega("re"),
+                notas.Pega("mi"),
+                notas.Pega("fa"),
+                notas.Pega("fa"),
+                notas.Pega("fa")
+            };
+            Piano piano = new Piano();
+            piano.Toca(musica);
         }
     }
 }
