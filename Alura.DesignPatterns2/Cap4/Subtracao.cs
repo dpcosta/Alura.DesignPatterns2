12c338f3922ae028a6c8138e60944974a38d2a30
dpcosta <dpcosta@gmail.com>
2018-04-02 14:04:29 -0300

    Arquivos da aula 4.

---
+++ Alura.DesignPatterns2/Cap4/Subtracao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.DesignPatterns2.Cap4
+{
+    class Subtracao : IExpressao
+    {
+        private IExpressao esquerda;
+        private IExpressao direita;
+
+        public Subtracao(IExpressao esquerda, IExpressao direita)
+        {
+            this.esquerda = esquerda;
+            this.direita = direita;
+        }
+
+        public int Avalia()
+        {
+            return this.esquerda.Avalia() - direita.Avalia();
+        }
+    }
+}
--- Alura.DesignPatterns2/Program.cs
+++ Alura.DesignPatterns2/Program.cs
@@ -1,11 +1,13 @@
 ﻿using Alura.DesignPatterns2.Cap1;
 using Alura.DesignPatterns2.Cap2;
 using Alura.DesignPatterns2.Cap3;
+using Alura.DesignPatterns2.Cap4;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +15,24 @@ namespace Alura.DesignPatterns2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Capitulo3();
+            Capitulo4();
+        }
+
+        static void Capitulo4()
+        {
+            // ( (1 + 100) + 10) + (20 - 10)
+            //IExpressao esquerda = new Soma(new Soma(new Numero(1), new Numero(100)), new Numero(10));
+            //IExpressao direita = new Subtracao(new Numero(20), new Numero(10));
+            //IExpressao soma = new Soma(esquerda, direita);
+
+            //Console.WriteLine(soma.Avalia());
+
+
+            Expression soma = Expression.Add(Expression.Constant(10), Expression.Constant(100));
+            Func<int> funcao = Expression.Lambda<Func<int>>(soma).Compile();
+            Console.WriteLine(funcao());
         }
 
         static void Capitulo3()
